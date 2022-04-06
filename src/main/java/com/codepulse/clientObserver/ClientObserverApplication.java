97fb2f74e5aa4553198ceea5889387da9bafbedb
conan <jinoh77@hotmail.com>
2022-04-06 13:54:41 +0000

    build: project build 환경 구성

--- src/main/java/com/codepulse/clientObserver/ClientObserverApplication.java
+++ src/main/java/com/codepulse/clientObserver/ClientObserverApplication.java
@@ -1,13 +1,17 @@
 package com.codepulse.clientObserver;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class ClientObserverApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ClientObserverApplication.class, args);
-	}
 
+		SpringApplication.run(ClientObserverApplication.class, args);
+
+		log.info("ClientObserverApplication is started.");
+	}
 }
