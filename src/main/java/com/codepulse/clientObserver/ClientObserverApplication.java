523f1f593e68d83103ea334b2cb62504547466e6
joyoo-conan <jinoh77@hotmail.com>
2022-04-06 18:53:19 +0900

    build: create project & commit

---
+++ src/main/java/com/codepulse/clientObserver/ClientObserverApplication.java
@@ -0,0 +1,13 @@
+package com.codepulse.clientObserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientObserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientObserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codepulse/clientObserver/ClientObserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codepulse.clientObserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientObserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
